394512b4f87554bfc48a9768cc30a06aeab4ab7f
mos9527 <greats3an@gmail.com>
2025-11-08 23:22:25 +0800

    Version 0.0.9 Revert 'Revert AssetStudio always add MonoBehaviour'

--- bindings/python/TypeTreeGeneratorAPI/__init__.py
+++ bindings/python/TypeTreeGeneratorAPI/__init__.py
@@ -1,6 +1,6 @@
 from .TypeTreeGenerator import TypeTreeBackend, TypeTreeGenerator, TypeTreeNode, TypeTreeNodeNative
 
-version = __version__ = "0.0.7"
+version = __version__ = "0.0.9"
 
 __all__ = [
     "__version__",
